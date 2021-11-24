7aa2505a4b6b4095fc1c1ea8fa18dbf4ceece849
tryoma <tryoma0326@gmail.com>
2021-11-24 11:10:46 +0900

    Initial Commit

---
+++ CodeJapan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeJapan
+//
+//  Created by 田久保竜馬 on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
